ebe7dd69a48d0c3290a79c25334174ec13dbf516
kiryls <kiryl92@gmail.com>
2020-05-22 19:41:28 +0200

    adding env file

--- src/dq.js
+++ src/dq.js
@@ -1,1 +1,5 @@
-console.log("Hello World!");
+require('dotenv').config();
+const Discord = require('discord.js');
+const client = new Discord.Client({ partials: ['MESSAGE', 'REACTION'] });
+
+console.log(process.env.BOT_TOKEN + '\nHello world!');
