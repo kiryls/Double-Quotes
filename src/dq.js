6b1fa67ee0d76e71a53f94eed7c58a9344ee2bf6
kiryls <kiryl92@gmail.com>
2020-05-22 19:47:05 +0200

    .env -> config.json

--- src/dq.js
+++ src/dq.js
@@ -1,5 +1,1 @@
-require('dotenv').config();
-const Discord = require('discord.js');
-const client = new Discord.Client({ partials: ['MESSAGE', 'REACTION'] });
-
-console.log(process.env.BOT_TOKEN + '\nHello world!');
+console.log('Hello world!');
